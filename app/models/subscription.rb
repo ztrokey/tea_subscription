e28492666fef849eb347595e7419b7398133fcaf
Zach T <ztrokey@gmail.com>
2021-09-20 14:02:50 -0600

    add subscription model

---
+++ app/models/subscription.rb
@@ -0,0 +1,7 @@
+class Subscription < ApplicationRecord
+  belongs_to :customer
+  belongs_to :tea
+
+  enum status: { pending: 0, active: 1, cancelled: 2 }
+  enum frequency: { weekly: 0, monthly: 1, quarterly: 2 }
+end
