849c7de80b9a0b96e98ccd5089b84bd13010ef1c
Zach T <ztrokey@gmail.com>
2021-09-20 14:04:45 -0600

    add tea relationship tests

---
+++ spec/models/tea_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Tea, type: :model do
+  describe 'relationships' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:customers).through(:subscriptions) }
+  end
+end
