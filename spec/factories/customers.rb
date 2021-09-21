06f1a5c2980341bf6ddafbad6a7e76ec16b065e3
Zach T <ztrokey@gmail.com>
2021-09-21 16:29:30 -0600

    run rubocop autocorrect

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :customer do
-    first_name { Faker::Name.first_name}
-    last_name { Faker::Name.last_name}
-    email { Faker::Internet.email}
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
     street_address { Faker::Address.street_address }
     city { Faker::Address.city }
     state { Faker::Address.state_abbr }
