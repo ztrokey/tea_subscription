9fa329947ca9abf1bc7fefe589a300da6c6976fa
Zach T <ztrokey@gmail.com>
2021-09-21 09:54:26 -0600

    create routes for subscription

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :customers, only: :show do
+        resources :subscriptions, only: :create
+      end
+    end
+  end
 end
